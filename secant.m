82e066a65ed29d96510162532eeb9f9e69d4d978
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-03-18 00:02:30 +0800

    Update secant.m

--- secant.m
+++ secant.m
@@ -1,14 +1,15 @@
 function root = secant(f, x0, x1, tol)
 
-while (abs(f(x1) - f(x0)) > 1)
-  h = plot(x1, f(x1), 'o');
-  x2 = x1 - f(x1)*(x1-x0)/(f(x1)-f(x0));
-  if (f(x2) * f(x0) < 0)
-    x1 = x2;
-  else
-    x0 = x2;
-  end
+while (abs(f(x1)) > tol)
+  h0 = plot(x0, f(x0), 'x');
+  h1 = plot(x1, f(x1), 'o');
+  l = plot([x0, x1], [f(x0), f(x1)], 'color', 'red');
+  x2 = x1 - f(x1) * (x1-x0) / (f(x1)-f(x0));
+  x0 = x1;
+  x1 = x2;
   pause;
-  delete(h);
+  delete(h0);
+  delete(h1);
+  delete(l);
 end
 root = x1;
