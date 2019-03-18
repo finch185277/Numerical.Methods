5ace5d4e98916d45fe71769abd526b22d9c2a802
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-03-18 09:12:26 +0800

    Update Newton.m

--- Newton.m
+++ Newton.m
@@ -1,11 +1,13 @@
 function root = Newton(f,df,x0,tol)
 while abs(f(x0)) > tol,
     x1 = x0 - f(x0)/df(x0);
-    h = plot(x1, f(x1), 'o');
-    l = line([x0, x1], [f(x0), 0], 'color', 'red');
+    h0 = plot(x0, f(x0), 'o');
+    h1 = plot(x1, f(x1), 'x');
+    l = line([x0, x1], [f(x0), f(x1)], 'color', 'red');
     x0 = x1;
     pause;
-    delete(h);
+    delete(h0);
+    delete(h1);
     delete(l);
 end
 root = x0;
