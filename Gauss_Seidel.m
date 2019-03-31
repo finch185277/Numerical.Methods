2f21d249c9bf17da2bcd3dffc42c81095c9cbfc9
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-03-31 14:52:56 +0800

    Add Gauss_Seidel.m

---
+++ Gauss_Seidel.m
@@ -0,0 +1,27 @@
+%% Gauss Seidel Method
+%% Solution of x in Ax=b using Gauss Seidel Method
+% * _*Initailize 'A' 'b' & intial guess 'x'*_
+function T = Gauss_Seidel(A, x, b, n)
+
+normVal=Inf;
+% * _*Tolerence for method*_
+tol=1e-5; itr=0;
+%% Algorithm: Gauss Seidel Method
+while normVal>tol
+    x_old=x;
+    for i=1:n
+        sigma=0;
+        for j=1:i-1
+                sigma=sigma+A(i,j)*x(j);
+        end
+        for j=i+1:n
+                sigma=sigma+A(i,j)*x_old(j);
+        end
+        x(i)=(1/A(i,i))*(b(i)-sigma);
+    end
+    itr=itr+1;
+    normVal=norm(x_old-x);
+end
+fprintf('Using Gauss Seidel method, solution of the system is: \n');
+disp(x);
+fprintf('in %d iterations\n', itr);
